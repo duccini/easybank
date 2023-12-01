122eac50ce942dd6844db3bb576d29b62754e8e2
Guilherme Duccini <guilhermeduccini@gmail.com>
2023-12-01 20:54:09 -0300

    finished menu

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,10 +1,35 @@
+import Image from "next/image";
 import styles from "./styles.module.css";
+
+import logo from "@/public/logo.svg";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="container">
       <header className={styles.menu}>
-        <h2>Meu Menu</h2>
+        <Image src={logo} height={25} alt="Easybank" quality={100} />
+        <div className={styles.navbar}>
+          <nav>
+            <Link href="/">
+              Home
+              <div className={styles.hover}></div>
+            </Link>
+            <Link href="/">
+              About <div className={styles.hover}></div>
+            </Link>
+            <Link href="/">
+              Contact <div className={styles.hover}></div>
+            </Link>
+            <Link href="/">
+              Blog <div className={styles.hover}></div>
+            </Link>
+            <Link href="/">
+              Carrers <div className={styles.hover}></div>
+            </Link>
+          </nav>
+        </div>
+        <button type="button">Request Invite</button>
       </header>
     </div>
   );
